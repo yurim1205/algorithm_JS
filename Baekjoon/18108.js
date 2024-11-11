f90cf854641177cec800f8a40ba28173e6aa46df
yurim1205 <eoulim3237@naver.com>
2024-11-11 18:22:07 +0900

    18108번

---
+++ Baekjoon/18108.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = process.platform === "linux" ? "/dev/stdin" : "/Users/iyulim/Desktop/algorithm/input.txt";
+const input = fs.readFileSync(filePath, 'utf-8').trim();
+
+console.log(input - 543);
