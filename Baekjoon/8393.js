dfcc3cf7e6e8583a95dbaf762c75c0f7510ab6ff
yurim1205 <eoulim3237@naver.com>
2024-11-26 15:55:48 +0900

    8393번

---
+++ Baekjoon/8393.js
@@ -0,0 +1,13 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "/Users/iyulim/Desktop/algorithm/input.txt")
+  .toString()
+  .split('\n'); // 줄 단위로 분리
+
+  const n = parseInt(input[0]);
+  let sum=0;
+
+  for(let i=1; i<=n; i++){
+    sum += i;
+  }
+
+  console.log(sum);
